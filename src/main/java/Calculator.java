b402209b1ef95ef80312b0d51c0f192bffef69bf
Anatolii Mitiliuk <anatoliimitiliuk@gmail.com>
2021-10-09 11:34:52 +0300

    First test

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+
+     public static int sum(int a, int b){
+        return a + b;
+    }
+
+     public static int mult(int a, int b){
+         return a * b;
+    }
+
+     public static double divide(int a, int b){
+        return a / b;
+    }
+}
